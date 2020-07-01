4d2dc42d1849deea505e4ad62fd0821ff54e810c
Guy-Norkunas <GuyCorneliusNorkunas@gmail.com>
2020-07-01 15:55:00 +1000

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from './components/navbar.component';
+import exerciseList from './components/exercises-list.component';
+import editExercise from './components/edit-exercises.component';
+import createExercise from './components/create-exercise.component';
+import createUser from './components/create-user.component';
 
 function App() {
   return (
